1a5ebf70d8b7c0cb8cc3a0343a0e4fae6403a6c8
Anton Skrebetz <antonskrebetz@gmail.com>
2021-02-16 20:50:07 +0300

    feat: ready task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array == undefined || array.length == 0) {
+        return 0;
+    }
+    if (array.length) {
+        let min = array[0];
+        for (i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array == undefined || array.length == 0) {
+        return 0;
+    }
+    if (array.length) {
+        let max = array[0];
+        for (i = 0; i < array.length; i++) {
+            if(array[i] > max) {
+            max = array[i];
+            }
+        }
+    return max;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array == undefined || array.length == 0) {
+        return 0;
+    }
+    if (array.length) {
+        let avg = 0;
+        for(i = 0; i < array.length; i++) {
+            avg += array[i]; 
+        }
+        return avg / array.length;
+    }
 }
